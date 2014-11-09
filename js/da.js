94aa7f1d81d189f1dbae6dc843458150c9545c07
Kyle Taylor <kyletaylored@gmail.com>
2014-11-09 17:27:25 -0600

    Lots more stuff

--- js/da.js
+++ js/da.js
@@ -20,6 +20,14 @@ jQuery(document).ready(function($) {
 	// Stellar.js
 	$.stellar();
 
+	// Forms.brace.io
+	$.ajax({
+    url: "//forms.brace.io/drawattentionco@gmail.com",
+    method: "POST",
+    data: {message: "hello!"},
+    dataType: "json"
+	});
+
 });
 
 /* === Smooth scroll === */
